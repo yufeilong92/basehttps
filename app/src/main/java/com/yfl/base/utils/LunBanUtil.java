b58a7a7e840ca1c640a4131d76e130c952d1163b
PC <931697478@qq.com>
2019-08-19 14:53:19 +0800

    更新分享工具，截屏工具

--- app/src/main/java/com/yfl/base/utils/LunBanUtil.java
+++ app/src/main/java/com/yfl/base/utils/LunBanUtil.java
@@ -68,7 +68,7 @@ public class LunBanUtil {
                             anInterface.imgError();
                         }
                     }
-                });
+                }).launch();
 
 
     }
