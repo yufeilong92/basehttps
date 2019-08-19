f0b300eace4602a93794a13f1dacd9c77874f7fb
PC <931697478@qq.com>
2019-08-19 14:58:11 +0800

    更新分享工具，截屏工具

--- yflLibrary/src/main/java/com/backpacker/yflLibrary/kotlin/GlideUtil.kt
+++ yflLibrary/src/main/java/com/backpacker/yflLibrary/kotlin/GlideUtil.kt
@@ -8,6 +8,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.UtilsLibrary.R
+import java.io.File
 
 /**
  * @Author : YFL  is Creating a porject in akhalteke-Android-driver
@@ -90,5 +91,80 @@ object GlideUtil {
                 .load(path)
                 .apply(mRequestOptions)
                 .into(img)
+    }
+
+    /**
+     * 加载图片
+     */
+    fun LoadBImager(context: Context, img: ImageView, path: String) {
+//        img.scaleType=ImageView.ScaleType.FIT_XY
+        if (StringUtil.isEmpty(path)){
+            img.setImageResource(R.mipmap.ic_default_picture)
+            return
+        }
+        Glide.with(context)
+            .load(File(path))
+            .into(img)
+    }
+    /**
+     * 加载图片
+     */
+    fun LoadBImagerWithOutHttp(context: Context, img: ImageView, path: String) {
+//        img.scaleType=ImageView.ScaleType.FIT_XY
+        if (StringUtil.isEmpty(path)){
+            img.setImageResource(R.mipmap.ic_default_picture)
+            return
+        }
+        Glide.with(context)
+            .load(File(path))
+            .into(img)
+    }
+
+    /**
+     * 加载四个圆角
+     */
+    fun loadBQuadRangleImager(context: Context, img: ImageView, path: String) {
+//        img.scaleType=ImageView.ScaleType.FIT_XY
+        if (StringUtil.isEmpty(path)){
+            img.setImageResource(R.mipmap.ic_default_picture)
+            return
+        }
+        val roundedCorners = RoundedCorners(10);
+        //通过RequestOptions扩展功能,override:采样率,因为ImageView就这么大,可以压缩图片,降低内存消耗
+//         RequestOptions options = RequestOptions.bitmapTransform(roundedCorners).override(300, 300);
+//        val override = RequestOptions.bitmapTransform(roundedCorners).override(300, 300)
+        val options = RequestOptions.bitmapTransform(roundedCorners);
+//        val float=8.0f
+//        val options = RequestOptions.bitmapTransform(
+//            RoundCorner(
+//                context,
+//                leftTop = float,
+//                rightBottom = float,
+//                rightTop = float,
+//                leftBottom = float
+//            )
+//        )
+        Glide.with(context)
+            .load(File(path))
+            .apply(options)
+            .into(img)
+    }
+
+    /**
+     * 加载圆角
+     */
+    fun loadBCilcleImager(context: Context, img: ImageView, path: String) {
+//        img.scaleType=ImageView.ScaleType.FIT_XY
+        if (StringUtil.isEmpty(path)){
+            img.setImageResource(R.mipmap.ic_default_picture)
+            return
+        }
+        val mRequestOptions = RequestOptions.circleCropTransform()
+            .diskCacheStrategy(DiskCacheStrategy.NONE)//不做磁盘缓存
+            .skipMemoryCache(true);//不做内存缓存
+        Glide.with(context)
+            .load(File(path))
+            .apply(mRequestOptions)
+            .into(img)
     }
 }
